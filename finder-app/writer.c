d671f3f72f0f48f8a9d25c0c1525b162b3dbffef
Chandana Challa <chch7974@colorado.edu>
2023-09-10 23:09:35 +0000

    moved result files to folder and added comments

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,10 +1,46 @@
+/*****************************************************************************
+ * Copyright (C) 2023 by Chandana Challa
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Chandana Challa and the University of Colorado are not liable for
+ * any misuse of this material.
+ *
+ *****************************************************************************/
+/**
+ * @file writer.c
+ * @brief This file contains functionality to write a string to a file.
+ *
+ * To compile: make
+ *
+ * @author Chandana Challa
+ * @date Sep 10 2023
+ * @version 1.0
+ * @resources AESD Course Slides
+ */
+
+/* Header files */
 #include <stdio.h>
 #include <syslog.h>
 #include <errno.h>
 #include <string.h>
 
+/* Macro definitions */
 #define ARG_COUNT    (3)
 
+/* Function definitions */
+
+/**
+ * @brief Prints usage of writer application
+ *
+ * Command format and the parameter details will be logged
+ * to syslog file
+ *
+ * @param void
+ *
+ * @return void
+ */
 void usage(void)
 {
     syslog(LOG_DEBUG, "Usage: ./writer <writeDir> <writeStr>");
@@ -14,6 +50,19 @@ void usage(void)
     syslog(LOG_DEBUG, "2)Text string to be written to a file.");
 }
 
+/**
+ * @brief Main function to write a string to the file
+ *
+ * Given a file path and string to write, it will validate the
+ * parameters and writes the string to the file and logs any errors
+ * or debug logs to syslog file.
+ *
+ * @param argc number of arguments
+ *
+ * @param argv array of command line arguments
+ *
+ * @return int - 1 on error, 0 on success.
+ */
 int main(int argc, char* argv[])
 {
     FILE *filePtr = NULL;
@@ -21,8 +70,10 @@ int main(int argc, char* argv[])
     const char *writeStr = NULL;
     int return_value = 0;
 
+    /* opens a connection to syslog for writing the logs */
     openlog(NULL, 0, LOG_USER);
 
+    /* validate the arguments */
     if (ARG_COUNT != argc)
     {
         syslog(LOG_ERR, "Invalid number of arguments %d", argc);
@@ -35,6 +86,7 @@ int main(int argc, char* argv[])
         writeStr = argv[2];
     }
 
+    /* open file in write mode */
     filePtr = fopen(writeDir, "w");
     if (NULL == filePtr)
     {
@@ -42,10 +94,12 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    /* write the string provided to the file */
     return_value = fwrite(writeStr, 1, strlen(writeStr), filePtr);
     if (strlen(writeStr) != return_value)
     {
-        syslog(LOG_ERR, "Error writing %s to %s file: %s", writeStr, writeDir, strerror(errno));
+        syslog(LOG_ERR, "Error writing %s to %s file: %s", writeStr, writeDir,
+               strerror(errno));
         return 1;
     }
     else
@@ -53,6 +107,9 @@ int main(int argc, char* argv[])
         syslog(LOG_DEBUG, "Writing %s to %s file success", writeStr, writeDir);
     }
  
+    /* close the file */
     fclose(filePtr);
+
+    /* close the connection to the syslog utility */
     closelog();
 }
